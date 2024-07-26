98c0093504e4e8736169f6414a9cee5dee97fd2d
revett <2796074+revett@users.noreply.github.com>
2024-07-26 21:50:39 +0100

    Dot files.

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  ignorePatterns: ["dist", ".eslintrc.cjs", "apps/shared/api/src/**/*.js"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "detect" } },
+  extends: [
+    "airbnb",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended",
+    "plugin:tailwindcss/recommended",
+  ],
+  plugins: ["react", "jsx-a11y", "import", "prettier", "tailwindcss"],
+  rules: {
+    "prettier/prettier": "error",
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "react/prop-types": "error",
+    "tailwindcss/classnames-order": "error",
+    "tailwindcss/no-custom-classname": "off",
+    "tailwindcss/no-contradicting-classname": "error",
+    "no-unused-vars": "error",
+  },
+}
